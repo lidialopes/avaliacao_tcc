98ad6f36a045705aeae967304d935a5a1331980a
felix-gustavo <gustavovieira1945@gmail.com>
2021-04-10 18:25:11 -0300

    add data

---
+++ src/database/data.ts
@@ -0,0 +1,50 @@
+export const tccData = [
+	{
+		id: "1",
+		tema: "A importância do uso da música no aprendizado"
+	},
+	{
+		id: "2",
+		tema: "Como tratar o bullying no ambiente escolar"
+	},
+	{
+		id: "3",
+		tema: "Desenvolvendo a auto-confiança na criança"
+	},
+	{
+		id: "4",
+		tema: "A Alfabetização de Jovens e Adultos"
+	},
+	{
+		id: "5",
+		tema: "Desenvolvendo a auto-confiança na criança"
+	}
+]
+
+export const professorData = [
+	{
+		id: "1",
+		nome: "Mauro"
+	},
+	{
+		id: "2",
+		nome: "Eva"
+	},
+	{
+		id: "3",
+		nome: "Osvaldo"
+	},
+	{
+		id: "4",
+		nome: "João Carlos"
+	},
+]
+
+export const alunoData = [
+	{
+		id: "1",
+		nome: "Joao",
+		matricula: "123123",
+		curso: "SI"
+	},
+]
