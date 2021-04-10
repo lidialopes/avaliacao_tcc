db7544e40eb7e5b0c78593e92404642d352c7357
felix-gustavo <gustavovieira1945@gmail.com>
2021-04-10 18:26:32 -0300

    create components

---
+++ src/components/AppButton.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+import { Button } from 'react-native-paper';
+
+export const AppButton = styled(Button)`
+  margin-top: 10px;
+`
---
+++ src/components/Separator.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+import { Divider } from 'react-native-paper';
+
+export const Separator = styled(Divider)`
+  background-color: black;
+`
