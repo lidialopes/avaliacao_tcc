17b8201cd2ffc4f334cdf5b9ac9a615a7a05ddfc
felix-gustavo <gustavovieira1945@gmail.com>
2021-04-10 18:28:08 -0300

    update Avaliacao page

--- src/pages/Avaliacao/index.tsx
+++ src/pages/Avaliacao/index.tsx
@@ -1,13 +1,138 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container, Title } from './styles';
+import { useRoute } from '@react-navigation/native';
+import { AppButton } from '../../components/AppButton';
+import { Container } from '../../components/Container'
+import { Separator } from '../../components/Separator'
+
+import {
+	AppTitle,
+	ListTitle,
+	TextTitle,
+	TccTitle,
+	TextInfo,
+	TextArea,
+	Header,
+	Content
+} from './styles';
+
+import { List } from 'react-native-paper';
+
+import { Tcc, Aluno, Professor } from '../../types/Types';
+import { alunoData, professorData } from '../../database/data';
 
 const Avaliacao: React.FC = () => {
-    return (
-        <Container>
-            <Title>Avaliação de Defesa</Title>
-        </Container>
-    );
+	const tcc = useRoute().params as Tcc;
+	const aluno = alunoData[0] as Aluno;
+	const professor = professorData[0] as Professor;
+
+	const [nConteudo, setNConteudo] = useState(0);
+	const [nRedacao, setNRedacao] = useState(0);
+	const [nNormalizacao, setNNormalizacao] = useState(0);
+	const [nCapacitacao, setNCapacitacao] = useState(0);
+	const [nResposta, setNResposta] = useState(0);
+	const [nPrevisao, setNPrevisao] = useState(0);
+	const [nota, setNota] = useState(0);
+
+	const calcAvg = () => {
+		setNota(((nConteudo +
+			nRedacao +
+			nNormalizacao +
+			nCapacitacao +
+			nResposta +
+			nPrevisao) / 2));
+	}
+
+	return (
+		<Container>
+			<AppTitle>Avaliação de Defesa</AppTitle>
+			<Content>
+				<Header>
+					<TccTitle>{tcc.tema}</TccTitle>
+					<TextInfo><TextTitle>Aluno: </TextTitle>{aluno.nome}</TextInfo>
+					<TextInfo><TextTitle>Matrícula: </TextTitle>{aluno.matricula}</TextInfo>
+					<TextInfo><TextTitle>Curso: </TextTitle>{aluno.curso}</TextInfo>
+					<TextInfo><TextTitle>Professor: </TextTitle>{professor.nome}</TextInfo>
+					<TextInfo><TextTitle>Nota: </TextTitle>{nota}</TextInfo>
+				</Header>
+
+				<ListTitle>Apresentação Escrita</ListTitle>
+				<Separator/>
+				<List.Item title="Conteúdo (0 a 5)" />
+				<TextArea label="Pontos" keyboardType="number-pad"
+					onChangeText={(e) => setNConteudo(Number(e))}
+					onEndEditing={() => calcAvg()}
+					
+				/>
+				<TextArea
+					label="Comentários"
+					multiline={true}
+					numberOfLines={1}
+					scrollEnabled={true}
+				/>
+				<List.Item title="Redação (0 a 3)" />
+				<TextArea label="Pontos" keyboardType="number-pad"
+					onChangeText={(e) => setNRedacao(Number(e))}
+					onEndEditing={() => calcAvg()}
+				/>
+				<TextArea
+					label="Comentários"
+					multiline={true}
+					numberOfLines={1}
+					scrollEnabled={true}
+				/>
+				<List.Item title="Normalização (0 a 2)" />
+				<TextArea label="Pontos" keyboardType="number-pad"
+					onChangeText={(e) => setNNormalizacao(Number(e))}
+					onEndEditing={() => calcAvg()}
+				/>
+				<TextArea
+					label="Comentários"
+					multiline={true}
+					numberOfLines={1}
+					scrollEnabled={true}
+				/>
+
+				<ListTitle>Apresentação Oral</ListTitle>
+				<Separator/>
+				<List.Item title="Capacitação de Exposição (0 a 5)" />
+				<TextArea label="Pontos" keyboardType="number-pad"
+					onChangeText={(e) => setNCapacitacao(Number(e))}
+					onEndEditing={() => calcAvg()}
+				/>
+				<TextArea
+					label="Comentários"
+					multiline={true}
+					numberOfLines={1}
+					scrollEnabled={true}
+				/>
+				<List.Item title="Respostas às Arguições (0 a 4)" />
+				<TextArea label="Pontos" keyboardType="number-pad"
+					onChangeText={(e) => setNResposta(Number(e))}
+					onEndEditing={() => calcAvg()}
+				/>
+				<TextArea
+					label="Comentários"
+					multiline={true}
+					numberOfLines={1}
+					scrollEnabled={true}
+				/>
+				<List.Item title="Previsão do Tempo (0 a 1)" />
+				<TextArea label="Pontos" keyboardType="number-pad"
+					onChangeText={(e) => setNPrevisao(Number(e))}
+					onEndEditing={() => calcAvg()}
+				/>
+				<TextArea
+					label="Comentários"
+					multiline={true}
+					numberOfLines={1}
+					scrollEnabled={true}
+				/>
+			</Content>
+
+			<AppButton mode="contained">Enviar</AppButton>
+		</Container>
+	);
 }
 
 export default Avaliacao;
--- src/pages/Avaliacao/styles.ts
+++ src/pages/Avaliacao/styles.ts
@@ -1,14 +1,58 @@
 import styled from 'styled-components/native';
+import { Text, TextInput } from 'react-native-paper';
 
-export const Container = styled.View`
-  flex: 1;
-  margin: 15px;
-`;
-
-export const Title = styled.Text`
+const AppTitle = styled.Text`
   font-family: Sans-Serif;
   font-size: 20px;
   color: #1d1d1d;
   text-align: center;
   font-weight: 700;
-`;
+  margin-bottom: 20px;
+`
+
+const ListTitle = styled(Text)`
+  font-size: 18px;
+  margin: 13px 0;
+`
+
+const TextTitle = styled(Text)`
+  font-weight: bold;
+`
+
+const TccTitle = styled(Text)`
+  text-align: center;
+  font-size: 16px;
+  font-weight: bold;
+  margin: 5px 0;
+`
+
+const TextInfo = styled(Text)`
+`
+
+const TextArea = styled(TextInput)`
+  max-height: 85px;
+  margin-bottom: 10px;
+`
+
+const Header = styled.View`
+  background-color: #eee;
+  margin-bottom: 10px;
+  padding: 14px;
+  border-radius: 5px;
+`
+
+
+const Content = styled.ScrollView`
+  padding-right: 10px;
+`
+
+export {
+  AppTitle,
+  ListTitle,
+  TextTitle,
+  TccTitle,
+  TextInfo,
+  TextArea,
+  Header,
+  Content
+}
