32fecd4887f21509cc720082d6de307151e54b07
felix-gustavo <gustavovieira1945@gmail.com>
2021-04-17 12:47:07 -0300

    update styles

--- src/components/AppButton.ts
+++ src/components/AppButton.ts
@@ -2,5 +2,6 @@ import styled from 'styled-components/native';
 import { Button } from 'react-native-paper';
 
 export const AppButton = styled(Button)`
-  margin-top: 10px;
+  margin: 10px 40px 40px;
+  padding: 2px 0;
 `
--- src/components/Container.ts
+++ src/components/Container.ts
@@ -2,5 +2,5 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  padding: 20px 40px;
+  justify-content: space-between;
 `;
